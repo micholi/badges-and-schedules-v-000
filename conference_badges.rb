fb3bbd08d2c2f1b2d4dbb4b352f9eca18b212d5e
Michelle Olivieri <michelleo72@gmail.com>
2018-03-20 15:46:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 
 def assign_rooms(array)
   array.each do |name|
-  room = array.each_with_index
+  array.each_with_index do |room, index|
   return "Hello, #{name}! You'll be assigned to room #{room[index]}!"
 end
 end
